d8890269f5643797fdb57ea90bc9afb282f2318f
Latoya Lofton <52177401+Toya88@users.noreply.github.com>
2020-01-10 13:15:30 -0600

    Changed title,  3rd butn static return butn

---
+++ js/app.js
@@ -0,0 +1,14 @@
+/*
+Need to input story text to screen
+Need story text to change based on where the player is in the story
+*Option to enter hero name
+*Option to pick geneder or ask their geneder/ story text will explain who they are now
+Need to input text on buttons
+Need first two buttons to receive new text options with each scene
+Need a way to pick 4 item choices - hidden buttons? dom generatored buttons that dissapear after
+Need a place to store the choices
+After receiving the list of items to collect, inventory system will appear to the left of the screen
+As each item is aquired, the name of the item on the list will change color indicating the receipt of the item
+
+
+*/
